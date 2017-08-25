da33fe8226e40fb25fece348f40bf5da06464ba9
中国天妖 <656861622@qq.com>
2017-08-25 10:41:58 +0800

    上传文件

---
+++ database/migrations/2017_08_24_171621_create_visit_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('salesman_id');
+            $table->integer('customer_id');
+            $table->longText('record');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
