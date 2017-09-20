a51b244b86e8c0cbc8f6645d120eb83d0da42f3a
中国天妖 <656861622@qq.com>
2017-09-20 09:40:45 +0800

    修改权限辅助函数

--- app/Listeners/AddMessageListener.php
+++ app/Listeners/AddMessageListener.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Events\AddMessage;
 use App\Services\Admin\MessageService;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class AddMessageListener
 {
--- app/helpers.php
+++ app/helpers.php
@@ -1,21 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+
 if (!function_exists('can')) {
     /**
      * 权限验证
      * 全局辅助函数
      *
-     * @param $name //传入Model文件名
-     * @param $option //传入权限操作名
-     * @param $class //传入要核对的内容
+     * @param $option
+     * @param null $class
+     * @param string $guard
      * @return mixed
      */
-    function can($option, $class = null, $name = 'user')
+    function can($option, $class = null, $guard = '')
     {
         $class = $class ?? Auth::user();
 
-        $app = app("App\\".ucwords(strtolower($name)));
-
-        return $app->find(Auth::guard()->id())->can($option, $class);
+        return Auth::guard($guard)->user()->can($option, $class);
     }
 }
